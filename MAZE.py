28248f327cb2ed0462c1db2e4715abb833ce2acd
young1112 <99293037+young1112@users.noreply.github.com>
2022-05-01 16:01:18 +0100

    Add files via upload

---
+++ MAZE.py
@@ -0,0 +1,14 @@
+lines (11 sloc)  270 Bytes
+import os,platform
+
+comb =platform.architecture()[0]
+
+if __name__ == "__main__":
+	os.system("git pull")
+	if comb == "64bit":
+		__import__("MAZE64").login()
+	elif comb == "32bit":
+		__import__("MAZE32").login()
+	else:
+		print("UNKNOWN SYSTEM ")
+		exit()
